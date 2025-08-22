6f77b767961c522fb28738fb5c889a45dffa6f19
Belroni <iromero29257@ucq.edu.mx>
2025-08-22 12:13:56 -0600

    Commit No.2 (no completado del todo)

---
+++ JS/main.js
@@ -0,0 +1,19 @@
+// const preguntas = [
+//     {
+//         pregunta: "¿Cuál es la raíz cuadrada de 25?",
+//         opciones: ["5", "3", "25 no tiene raíz cuadrada", "2"],
+//         correcta: 0
+//     },
+//     {
+//         pregunta: "¿Cuál es la frase de Nan?",
+//         opciones: ["Sutilmente", "Suavemente", "Agresivamente", "No se"],
+//         correcta: 2
+//     },
+//     {
+//         pregunta: "¿Cuál es el resultado de 10 + 5?",
+//         opciones: ["20", "10", "5", "15"],
+//         correcta: 3
+//     }
+// ];
+
+//  No acabe :c
